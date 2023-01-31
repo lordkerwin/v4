2e00aa7bdb17b076eded50e215e5b39c3da847dd
Sean Kerwin <swkerwin@icloud.com>
2023-01-31 16:30:16 +0000

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
